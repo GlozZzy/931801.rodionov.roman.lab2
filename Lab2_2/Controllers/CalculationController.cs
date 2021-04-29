d40bf8818a19ee1bb2291ff953781b4a99690a46
Roman Rodionov <kpackl14@gmail.com>
2021-04-29 23:44:42 +0700

    Добавьте файлы проекта.

---
+++ Lab2_2/Controllers/CalculationController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2_2.Models;
+
+namespace Lab2_2.Controllers
+{
+    public class CalculationController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalculationController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult ManualSingle()
+        {
+            if (Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                CalculationModel calc = new CalculationModel();
+                if (Request.Form["Val1"] == "" && Request.Form["Val2"] == "")
+                {
+                    calc.Val1 = 0;
+                    calc.Val2 = 0;
+                }
+                else if (Request.Form["Val1"] != "" && Request.Form["Val2"] == "")
+                {
+                    calc.Val1 = Int32.Parse(Request.Form["Val1"]); 
+                    calc.Val2 = 0;
+                }
+                else if (Request.Form["Val1"] == "" && Request.Form["Val2"] != "")
+                {
+                    calc.Val1 = 0;
+                    calc.Val2 = Int32.Parse(Request.Form["Val2"]);
+                }
+                else
+                {
+                    calc.Val1 = Int32.Parse(Request.Form["Val1"]);
+                    calc.Val2 = Int32.Parse(Request.Form["Val2"]);
+                }
+
+                calc.Operation = Request.Form["Operation"];
+                calc.calc_Result();
+                ViewBag.Result = calc.Result;
+                return View("Result");
+            }
+            else return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ManualSeparate()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("ManualSeparate")]
+        public IActionResult ManualSeparatePost()
+        {
+            CalculationModel calc = new CalculationModel();
+            if (Request.Form["Val1"] == "" && Request.Form["Val2"] == "")
+            {
+                calc.Val1 = 0;
+                calc.Val2 = 0;
+            }
+            else if (Request.Form["Val1"] != "" && Request.Form["Val2"] == "")
+            {
+                calc.Val1 = Int32.Parse(Request.Form["Val1"]);
+                calc.Val2 = 0;
+            }
+            else if (Request.Form["Val1"] == "" && Request.Form["Val2"] != "")
+            {
+                calc.Val1 = 0;
+                calc.Val2 = Int32.Parse(Request.Form["Val2"]);
+            }
+            else
+            {
+                calc.Val1 = Int32.Parse(Request.Form["Val1"]);
+                calc.Val2 = Int32.Parse(Request.Form["Val2"]);
+            }
+
+            calc.Operation = Request.Form["Operation"];
+            calc.calc_Result();
+            ViewBag.Result = calc.Result;
+            return View("Result");
+        }
+
+
+        [HttpGet]
+        public IActionResult ModelParam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ModelParam(int Val1, int Val2, string Operation)
+        {
+            CalculationModel calc = new CalculationModel();
+            calc.Val1 = Val1;
+            calc.Val2 = Val2;
+            calc.Operation = Operation;
+            calc.calc_Result();
+            ViewBag.Result = calc.Result;
+            return View("Result");
+        }
+
+
+        [HttpGet]
+        public IActionResult ModelSeparate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ModelSeparate(CalculationModel calc)
+        {
+            calc.calc_Result();
+            ViewBag.Result = calc.Result;
+            return View("Result");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
---
+++ Lab2_2/Models/CalculationModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_2.Models
+{
+    public class CalculationModel
+    {
+        public int Val1 { get; set; }
+        public int Val2 { get; set; }
+        public string Operation { get; set; }
+        public string Result { get; set; }
+
+        public void calc_Result()
+        {
+            Result = "" + Val1;
+            switch (Operation)
+            {
+                case "+":
+                    Result += " + " + Val2 + " = " + (Val1 + Val2);
+                    break;
+                case "-":
+                    Result += " - " + Val2 + " = " + (Val1 - Val2);
+                    break;
+                case "*":
+                    Result += " * " + Val2 + " = " + (Val1 * Val2);
+                    break;
+                case "/":
+                    Result += " / " + Val2 + " = ";
+                    if (Val2 != 0) Result += (Val1 / Val2);
+                    else Result += "ERROR: Division by zero";
+                    break;
+            }
+        }
+    }
+}
